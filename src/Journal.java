26e5cf15f923a5c76ae25d9e6d89187c56e5e9c5
amy-upthagrove <amyupthagrove@gmail.com>
2022-03-08 16:27:04 -0800

    create Player and Journal classes. AB#104

---
+++ src/Journal.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class Journal {
+    public static void main(String[] args){
+        ArrayList<String> clues = new ArrayList<String>();
+        clues.add("go to the library!");
+        clues.add("storm area 51!");
+        clues.add("moooo!");
+        System.out.println(clues);
+    }
+}
