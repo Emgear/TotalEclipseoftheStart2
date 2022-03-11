4388cf9fbaa751ac94563aa9740dd4e3d88f937f
Julian <julians8522@gmail.com>
2022-03-11 09:31:20 -0800

    Player is able to view inventory AB#125

--- src/com/totaleclipse/player/Journal.java
+++ src/com/totaleclipse/player/Journal.java
@@ -1,24 +1,32 @@
-//package com.totaleclipse.player;
-//
-//import java.util.ArrayList;
-//
-//public class Journal {
-//    public String getClue() {
-//        return clue;
-//    }
-//
-//    public void setClue(String clue) {
-//        this.clue = clue;
-//    }
-//
-//    String clue;
-//    //won't be main method, this is just for testing
-//    public static void main(String[] args){
-//        ArrayList<String> clues = new ArrayList<>();
-//        // instance variable string clue
-//        clues.add("go to the library!");
-//        clues.add("storm area 51!");
-//        clues.add("moooo!");
-//        System.out.println(clues);
-//    }
-//}
+package com.totaleclipse.player;
+
+import com.totaleclipse.clues.Clue;
+
+import java.util.*;
+
+public class Journal {
+
+    private Journal(){}
+
+    private static Map<String, Clue> clues = new HashMap<>();
+
+    public static Map<String, Clue> getClues() {
+        return clues;
+    }
+
+    public static void addClue(String source, Clue clue) {
+        clues.put(source, clue);
+    }
+
+    public static String getClueString(){
+        StringBuilder cluesString = new StringBuilder();
+
+        for(String source : clues.keySet()){
+            cluesString.insert(0,source + ": " + clues.get(source).getClue() + '\n');
+        }
+
+        cluesString.insert(0,"Current clues: \n");
+        return cluesString.toString();
+    }
+
+}
