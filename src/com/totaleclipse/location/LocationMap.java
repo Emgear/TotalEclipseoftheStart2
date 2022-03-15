c408dd7548476c1c08b14b7ec48be84140e5baad
Julian <julians8522@gmail.com>
2022-03-15 07:45:31 -0700

    Formatted output of map. AB#261 and AB#135

--- src/com/totaleclipse/location/LocationMap.java
+++ src/com/totaleclipse/location/LocationMap.java
@@ -1,13 +1,19 @@
 package com.totaleclipse.location;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LocationMap {
 
-    private static LocationMap map;
+    private static LocationMap map; //map object for singleton
 
     //private constructor to prevent outside instantiation
     private LocationMap(){}
 
-    //singleton factory, only one instance of map is ever needed, if any
+    /**
+     * Singleton factory for this map. Only one instance is ever needed.
+     * @return The single instance of LocationMap
+     */
     public static LocationMap getInstance(){
         if(map == null){
             map = new LocationMap();
@@ -15,12 +21,44 @@ public class LocationMap {
         return map;
     }
 
+    /**
+     * A partially-scalable map output the displays the locations of the map
+     */
     public void printMap(){
-        System.out.println(Locations.locationsMap.get(10).getLocation()+"                        "+Locations.locationsMap.get(9).getLocation()+"                         "+Locations.locationsMap.get(8).getLocation());
-        System.out.println(Locations.locationsMap.get(7).getLocation()+"                       "+Locations.locationsMap.get(6).getLocation()+"                          "+Locations.locationsMap.get(5).getLocation());
-        System.out.println(Locations.locationsMap.get(4).getLocation()+"                     "+Locations.locationsMap.get(3).getLocation()+"                           "+Locations.locationsMap.get(2).getLocation());
-        System.out.println("                                 "+Locations.locationsMap.get(1).getLocation());
-        System.out.println("                                 "+Locations.locationsMap.get(0).getLocation());
 
+        //Add all strings to a list to help with readability
+        List<String> names = new ArrayList<>();
+
+        //scalable with size
+        for(int i = Locations.locationsMap.size() - 2; i > 1; i--){
+            names.add(Locations.locationsMap.get(i).getLocation());
+        }
+
+        //map height is determined based on the width of the map, not including the 3 static locations
+        //number of locations in each row of the map
+        int mapWidth = 3;
+        int mapHeight = names.size() / mapWidth;
+
+        //top of map
+        System.out.println("-------------------------------------------------------------------------------------------------------------------------------");
+
+        //fill each random location in map
+        for(int i = 0; i < mapHeight; i++){
+            List<String> row = new ArrayList<>();
+            for(int x = 0; x < mapWidth; x++){
+                int index = x + (mapWidth *i);
+                row.add(names.get(index));
+            }
+            System.out.format("|%32s%32s%32s%32s", row.get(0), row.get(1), row.get(2), "|\n");
+            System.out.format("|%126s|\n", "");
+        }
+
+        //Static locations
+        System.out.format("|%96s%32s", Locations.locationsMap.get(1).getLocation(), "|\n");
+        System.out.format("|%126s|\n", "");
+        System.out.format("|%96s%32s", Locations.locationsMap.get(0).getLocation(), "|\n");
+
+        //bottom of map
+        System.out.println("-------------------------------------------------------------------------------------------------------------------------------\n");
     }
 }
