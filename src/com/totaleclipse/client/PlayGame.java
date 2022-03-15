0cf13d2ee05f37a1afb9a4b847cd59188de89717
Julian <julians8522@gmail.com>
2022-03-15 07:52:59 -0700

    Deleted extraneous comment and method.

--- src/com/totaleclipse/client/PlayGame.java
+++ src/com/totaleclipse/client/PlayGame.java
@@ -27,8 +27,6 @@ public class PlayGame {
                 "Uncover your past and discover your true identity!");
         DisplayScreen.displayConsole("To exit the game, simply type \"quit\"\n\n");
 
-        //player types start.
-
         //Explaining the start of the game
         DisplayScreen.displayConsole("You wake up in a crop circle, surrounded by corn. Your leather jacket is dirty.\nYou can't seem to remember who you are or how you ended up in the midst of all this corn, and you want answers.\nOver the corn in the distance, you can see a cow floating up into a disc-like shape in the sky, before it flies away.\nType out the action you wish to perform, with a verb first an noun second.\nFor example, you can look around by typing \"look north\" or \"look west\".\nFor more commands, type \"help\".");
 
