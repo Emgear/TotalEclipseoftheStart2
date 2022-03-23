7beac95d586cffe0672faf0a73364ec70daa6c47
Sanil Pannachan <spannachan@gmail.com>
2022-03-23 09:31:52 -0500

    game logo issue, therefore commented out, AB#321

---
+++ test/com/totaleclipse/player/PlayerTest.java
@@ -0,0 +1,57 @@
+package com.totaleclipse.player;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PlayerTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void getInstance() {
+    }
+
+    @Test
+    public void getHumanity() {
+    }
+
+    @Test
+    public void setHumanity() {
+    }
+
+    @Test
+    public void getClue() {
+    }
+
+    @Test
+    public void setClue() {
+    }
+
+    @Test
+    public void getPlayerName() {
+    }
+
+    @Test
+    public void getLocation() {
+    }
+
+    @Test
+    public void setLocation() {
+    }
+
+    @Test
+    public void getPlayerHp() {
+    }
+
+    @Test
+    public void setPlayerHp() {
+    }
+
+    @Test
+    public void getEnemy() {
+    }
+}
