e6cec923e79b318e0635aba0518948656fe19d7e
Sanil Pannachan <spannachan@gmail.com>
2022-03-23 09:47:31 -0500

    test class for player class

--- test/com/totaleclipse/player/PlayerTest.java
+++ test/com/totaleclipse/player/PlayerTest.java
@@ -1,57 +1,56 @@
 package com.totaleclipse.player;
 
+import com.totaleclipse.clues.Clue;
+import com.totaleclipse.enemy.Enemy;
+import com.totaleclipse.location.Location;
 import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class PlayerTest {
+    Location loc1 = new Location();
+    Clue clue1 = new Clue();
+    Enemy enemy1 = new Enemy();
+    Player p1 = Player.getInstance("Bob",loc1, clue1, 50, enemy1);
 
-    @Before
-    public void setUp() throws Exception {
-    }
 
     @Test
     public void getInstance() {
-    }
-
-    @Test
-    public void getHumanity() {
+        Player player = Player.getInstance("Bob",loc1, clue1, 50, enemy1);
+        assertNotNull("Player class exists", player);
     }
 
     @Test
     public void setHumanity() {
+        p1.setHumanity(50);
+        assertEquals(50, p1.getHumanity());
     }
 
     @Test
     public void getClue() {
+        assertNotNull("Player class exists", p1.getClue());
     }
 
-    @Test
-    public void setClue() {
-    }
 
     @Test
     public void getPlayerName() {
+        assertEquals("Bob", p1.getPlayerName());
     }
 
     @Test
     public void getLocation() {
-    }
+        assertNotNull("Player class exists", p1.getLocation());
 
-    @Test
-    public void setLocation() {
     }
 
     @Test
     public void getPlayerHp() {
-    }
-
-    @Test
-    public void setPlayerHp() {
+        assertEquals(50, p1.getPlayerHp());
     }
 
     @Test
     public void getEnemy() {
+        assertNotNull("Player class exists", p1.getEnemy());
     }
 }
